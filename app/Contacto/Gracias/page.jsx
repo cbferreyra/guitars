8c0530ac95611d3867dc9cfe6a1cf0d81b8d7b7d
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-08-22 20:12:04 -0300

    agregado formulario para netlify

---
+++ app/Contacto/Gracias/page.jsx
@@ -0,0 +1,8 @@
+export default function Agradecimiento() {
+  return (
+    <div className="agradecimientoMensaje">
+      <h2>muchas gracias por tu mensaje</h2>
+      <p>Te responderemos a la brevedad</p>
+    </div>
+  );
+}
--- app/Contacto/page.jsx
+++ app/Contacto/page.jsx
@@ -6,8 +6,56 @@ export default function Contacto() {
       <div>
         <h1 className="tituloContacto">Contactanos</h1>
       </div>
+      {/* formulario para Netlify */}
 
-      <form className="contacto__form" method="" action="">
+      <form
+        name="contacto"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        hidden
+      >
+        <input
+          className="fieldText"
+          type="text"
+          name="nombre"
+          id="nombre"
+          placeholder="Nombre"
+        />
+
+        <input
+          className="fieldText"
+          type="text"
+          name="apellido"
+          id="apellido"
+          placeholder="Apellido"
+        />
+
+        <input
+          className="fieldText"
+          type="email"
+          name="correo"
+          id="correo"
+          placeholder="e-mail"
+        />
+
+        <div className="textArea__label"></div>
+        <textarea
+          class="textarea"
+          name="mensaje"
+          id="mensaje"
+          cols="30"
+          rows="10"
+          placeholder="Escribí aquí tu mensaje"
+        ></textarea>
+
+        {/* <div className="checkAceptacion">
+          <input type="checkbox" name="acepta" id="acepta" value="1" /> Acepto
+          los términos y condiciones
+        </div> */}
+      </form>
+
+      {/* formulario posta */}
+      <form className="contacto__form" name="contacto" method="">
         <legend class="form__legend">Contacto</legend>
 
         <input
@@ -34,14 +82,6 @@ export default function Contacto() {
           placeholder="e-mail"
         />
 
-        <input
-          className="fieldText"
-          type="tel"
-          name="telefono"
-          id="telefono"
-          placeholder="celular"
-        />
-
         <div className="textArea__label"></div>
         <textarea
           class="textarea"
@@ -54,10 +94,10 @@ export default function Contacto() {
 
         <input type="submit" value="Enviar" />
 
-        <div className="checkAceptacion">
+        {/* <div className="checkAceptacion">
           <input type="checkbox" name="acepta" id="acepta" value="1" /> Acepto
           los terminosycondiciones términos y condiciones
-        </div>
+        </div> */}
       </form>
     </div>
   );
