fae3cde625bd298159cceb5e61415a5fb9770049
Cristian Ferreyra <cristianbferreyra@gmail.com>
2023-08-24 15:16:39 -0300

    probando cruzando dedos

--- app/Contacto/page.jsx
+++ app/Contacto/page.jsx
@@ -6,72 +6,35 @@ export default function Contacto() {
       <div>
         <h1 className="tituloContacto">Contactanos</h1>
       </div>
-      {/* formulario para Netlify otro intento */}
 
+      {/* formulario para Netlify otro intento */}
       <form
         name="contacto"
         data-netlify="true"
         netlify-honeypot="bot-field"
         hidden
       >
-        <input
-          /*  className="fieldText" */
-          type="text"
-          name="nombre"
-        />
-
-        <input
-          /*  */ className="fieldText"
-          type="text"
-          name="apellido"
-          id="apellido"
-          placeholder="Apellido"
-        />
-
-        <input
-          className="fieldText"
-          type="email"
-          name="correo"
-          id="correo"
-          placeholder="e-mail"
-        />
-
-        <textarea class="textarea" name="mensaje"></textarea>
+        <input type="text" name="nombre" />
+        <input type="email" name="email" />
+        <textarea name="comentario"></textarea>
       </form>
 
-      {/* formulario posta */}
-
-      <form method="post" className="contacto__form">
-        <legend class="form__legend">Contacto</legend>
-
-        <input
-          className="fieldText"
-          type="text"
-          name="nombre"
-          id="nombre"
-          placeholder="Nombre"
-        />
-
-        <input
-          className="fieldText"
-          type="text"
-          name="apellido"
-          id="apellido"
-          placeholder="Apellido"
-        />
-
-        <input
-          className="fieldText"
-          type="email"
-          name="correo"
-          id="correo"
-          placeholder="e-mail"
-        />
+      <form method="post" className="formulario">
         <label>
+          Nombre:
+          <input type="text" name="nombre" />
+        </label>
+        <label>
+          Mail:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Comentarios:
           <textarea name="comentario"></textarea>
         </label>
 
-        <input type="hidden" name="form name" value="contacto" />
+        <input type="hidden" name="form-name" value="contacto" />
+
         <button type="submit">Enviar"</button>
       </form>
     </div>
